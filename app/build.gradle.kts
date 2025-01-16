09c166b15bb150edf7d7c988b6a28afaee1f13ab
dirty-dan01 <guilledaniel_1@hotmail.com>
2025-01-16 11:16:50 -0500

    Project setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,9 @@ android {
 }
 
 dependencies {
-
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
